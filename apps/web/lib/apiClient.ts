5fcd86b71617b89c3f4bea59494f2ce25878cc80
krit22 <kritkumar2@gmail.com>
2026-06-10 17:07:40 +0000

    fixing cookie erroe v1

--- apps/web/lib/apiClient.ts
+++ apps/web/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios, { isAxiosError } from "axios";
 import { useAuthStore } from "@/store/auth/store";
 
 export const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "/api/v1",
     withCredentials: true, // Sends secure cookies automatically
     headers: {
         "Content-Type": "application/json",
